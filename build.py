247d90fdc22291ccf7aecbe117e588de05cb5478
micbou <contact@micbou.com>
2015-11-24 23:10:44 +0100

    Improve build script
    Add sane defaults to Vim build.

--- build.py
+++ build.py
@@ -8,13 +8,8 @@ import platform
 SCRIPT_DIR = os.path.dirname(os.path.abspath( __file__ ))
 SOURCES_DIR = os.path.join(SCRIPT_DIR, 'src')
 
-MSVC11_ROOT_DIR = r'C:\Program Files (x86)\Microsoft Visual Studio 11.0'
-MSVC12_ROOT_DIR = r'C:\Program Files (x86)\Microsoft Visual Studio 12.0'
-MSVC14_ROOT_DIR = r'C:\Program Files (x86)\Microsoft Visual Studio 14.0'
-
-MSVC_BIN_DIR = os.path.join('VC', 'bin')
-VC_VARS_32_SCRIPT = 'vcvars32.bat'
-VC_VARS_64_SCRIPT = os.path.join('x86_amd64', 'vcvarsx86_amd64.bat')
+MSVC_BIN_DIR = os.path.join('..', '..', 'VC', 'bin')
+VC_VARS_SCRIPT = os.path.join('..', '..', 'VC', 'vcvarsall.bat')
 
 SDK_INCLUDE_DIR = ( r'C:\Program Files (x86)\Microsoft SDKs\Windows'
                     r'\v7.1A\Include' )
@@ -56,31 +51,43 @@ def get_pythons_build_args(args):
 
 def get_msvc_dir(args):
     if args.msvc == 11:
-        msvc_dir = os.path.join(MSVC11_ROOT_DIR, MSVC_BIN_DIR)
+        return os.path.join(os.environ['VS110COMNTOOLS'], MSVC_BIN_DIR)
     if args.msvc == 12:
-        msvc_dir = os.path.join(MSVC12_ROOT_DIR, MSVC_BIN_DIR)
+        return os.path.join(os.environ['VS120COMNTOOLS'], MSVC_BIN_DIR)
     if args.msvc == 14:
-        msvc_dir = os.path.join(MSVC14_ROOT_DIR, MSVC_BIN_DIR)
-    if not os.path.exists(msvc_dir):
-        raise RuntimeError('{0} folder does not exist. Did you install '
-                           'Microsoft Visual C++ {1}?'.format(msvc_dir,
-                                                              args.msvc))
-    return msvc_dir
+        return os.path.join(os.environ['VS140COMNTOOLS'], MSVC_BIN_DIR)
+    raise RuntimeError('msvc parameter should be 11, 12, or 14.')
 
 
-def get_vc_vars_script_path(arch, msvc_dir):
+def get_vc_mod(arch):
     if arch == 64:
-        return os.path.join(msvc_dir, VC_VARS_64_SCRIPT)
-    return os.path.join(msvc_dir, VC_VARS_32_SCRIPT)
+        return 'x86_amd64'
+    return 'x86'
+
+
+def get_build_args(args, gui=True):
+    build_args = [get_arch_build_args(args)]
+
+    if gui:
+        build_args.extend(['GUI=yes',
+                           'OLE=yes',
+                           'IME=yes',
+                           'GIME=yes',
+                           'DIRECTX=yes'])
+
+    build_args.extend(['WINVER=0x0500',
+                       'FEATURES=HUGE',
+                       'MBYTE=yes',
+                       'ICONV=yes',
+                       'DEBUG=no'])
+
+    build_args.extend(get_pythons_build_args(args))
+
+    return build_args
 
 
 def build_vim(args, gui = True):
     os.chdir(SOURCES_DIR)
-
-    msvc_dir = get_msvc_dir(args)
-
-    vc_vars_script_path = get_vc_vars_script_path(args.arch, msvc_dir)
-    build_cmd = [vc_vars_script_path]
 
     new_env = os.environ.copy()
 
@@ -89,25 +96,26 @@ def build_vim(args, gui = True):
 
     new_env['SDK_INCLUDE_DIR'] = SDK_INCLUDE_DIR
 
+    msvc_dir = get_msvc_dir(args)
+
     nmake = os.path.join(msvc_dir, 'nmake.exe')
     if not os.path.exists(nmake):
         raise RuntimeError('nmake tool not found.')
 
+    build_args = get_build_args(args, gui)
+
+    clean_cmd = [nmake, '/f', 'Make_mvc.mak', 'clean']
+    clean_cmd.extend(build_args)
+
+    subprocess.call(clean_cmd, env = new_env)
+
+    vc_vars_script_path = os.path.join(msvc_dir, VC_VARS_SCRIPT)
+
+    build_cmd = [vc_vars_script_path, get_vc_mod(args.arch)]
     build_cmd.extend(['&',
-                      nmake, '-f',
+                      nmake, '/f',
                       'Make_mvc.mak'])
-
-    build_cmd.append(get_arch_build_args(args))
-
-    if gui:
-        build_cmd.extend(['GUI=yes',
-                          'IME=yes'])
-
-    build_cmd.extend(['MBYTE=yes',
-                      'ICONV=yes',
-                      'DEBUG=no'])
-
-    build_cmd.extend(get_pythons_build_args(args))
+    build_cmd.extend(build_args)
 
     subprocess.check_call(build_cmd, env = new_env)
 
@@ -130,8 +138,8 @@ def parse_arguments():
                         help = 'Set python2 folder (default: C:\Python27 or '
                         'C:\Python27-64 depending on architecture)')
     parser.add_argument('--python3', type = str,
-                        help = 'Set python3 folder (default: C:\Python35 or '
-                        'C:\Python35-64 depending on architecture)')
+                        help = 'Set python3 folder (default: C:\Python34 or '
+                        'C:\Python34-64 depending on architecture)')
 
     args = parser.parse_args()
     if not args.arch:
