7005b962f713c1f0ac70d60764f8978c25d48ef3
micbou <contact@micbou.com>
2015-11-25 14:56:04 +0100

    Automatically add files to download list

--- appveyor/bintray.py
+++ appveyor/bintray.py
@@ -4,26 +4,52 @@ import argparse
 import requests
 import os
 
-BINTRAY_URL = ( 'https://api.bintray.com/content/'
-                '{subject}/'
-                '{repo}/'
-                '{package}/'
-                '{version}/'
-                '{file_path}' )
+BINTRAY_BASE_URL = 'https://api.bintray.com/'
+BINTRAY_UPLOAD_URL = (BINTRAY_BASE_URL +
+                      'content/'
+                      '{subject}/'
+                      '{repo}/'
+                      '{package}/'
+                      '{version}/'
+                      '{file_path}')
+BINTRAY_PUBLISH_URL = (BINTRAY_BASE_URL +
+                       'content/'
+                       '{subject}/'
+                       '{repo}/'
+                       '{package}/'
+                       '{version}/'
+                       'publish')
+BINTRAY_DOWNLOAD_LIST_URL = (BINTRAY_BASE_URL +
+                             'file_metadata/'
+                             '{subject}/'
+                             '{repo}/'
+                             '{file_path}')
+BINTRAY_VERSION_URL = (BINTRAY_BASE_URL +
+                       'packages/'
+                       '{subject}/'
+                       '{repo}/'
+                       '{package}/'
+                       'versions/'
+                       '{version}')
 
 
-def upload_to_bintray(args):
+
+def bintray_upload(args):
+    args.filepath = ( args.filepath if args.filepath else
+                      os.path.basename(args.file_input) )
+
     params = {
         'publish': 1 if args.publish else 0,
         'override': 1 if args.override else 0,
         'explode': 1 if args.explode else 0
     }
 
-    url = BINTRAY_URL.format(subject = args.subject,
-                             repo = args.repo,
-                             package = args.package,
-                             version = args.version,
-                             file_path = args.filepath)
+    url = BINTRAY_UPLOAD_URL.format(subject = args.subject,
+                                    repo = args.repo,
+                                    package = args.package,
+                                    version = args.version,
+                                    file_path = args.filepath)
+    print(url)
 
     with open(args.file_input, 'rb') as f:
         response = requests.put(url,
@@ -33,48 +59,243 @@ def upload_to_bintray(args):
                                         args.api_key))
 
     if response.status_code != 201:
-        print('Bintray upload failed with message: {0}'
-              .format(response.text))
+        raise RuntimeError('Bintray upload failed with message: {0}'
+                           .format(response.text))
+
+
+def bintray_publish(args):
+    json = {
+        'discard': True if args.discard else False,
+        'publish_wait_for_secs': -1
+    }
+
+    url = BINTRAY_PUBLISH_URL.format(subject = args.subject,
+                                     repo = args.repo,
+                                     package = args.package,
+                                     version = args.version)
+    print(url)
+
+    response = requests.post(url,
+                             json = json,
+                             auth = (args.username,
+                                     args.api_key))
+
+    if response.status_code != 200:
+        raise RuntimeError('Publish failed with status code: {0}'
+                           .format(response.status_code))
+
+
+def bintray_file_in_download_list(args):
+    json = {
+        'list_in_downloads': True if args.action == 'add' else False
+    }
+
+    url = BINTRAY_DOWNLOAD_LIST_URL.format(subject = args.subject,
+                                           repo = args.repo,
+                                           file_path = args.filepath)
+    print(url)
+
+    response = requests.put(url,
+                            json = json,
+                            auth = (args.username,
+                                    args.api_key))
+
+    if response.status_code != 200:
+        action = 'Adding' if args.action == 'add' else 'Removing'
+        raise RuntimeError('{0} file in download list failed with status '
+                           'code: {1}'.format(action, response.status_code))
+
+
+def delete_version(args):
+    url = BINTRAY_VERSION_URL.format(subject = args.subject,
+                                     repo = args.repo,
+                                     package = args.package,
+                                     version = args.version)
+    print(url)
+
+    response = requests.delete(url,
+                               auth = (args.username,
+                                       args.api_key))
+
+    if response.status_code != 200:
+        raise RuntimeError('Deleting version failed with status code: {0}'
+                           .format(response.status_code))
+
+
+def update_version(args):
+    json = {
+        'desc': args.desc,
+    }
+
+    if args.github_release_notes_file:
+        json['github_release_notes_file'] = args.github_release_notes_file
+    if args.github_use_tag_release_notes:
+        json['github_use_tag_release_notes'] = (
+            args.github_use_tag_release_notes)
+    if args.vcs_tag:
+        json['vcs_tag'] = args.vcs_tag
+    if args.released:
+        json['released'] = args.released
+
+    url = BINTRAY_VERSION_URL.format(subject = args.subject,
+                                     repo = args.repo,
+                                     package = args.package,
+                                     version = args.version)
+    print(url)
+
+    response = requests.patch(url,
+                              json = json,
+                              auth = (args.username,
+                                      args.api_key))
+
+    if response.status_code != 200:
+        raise RuntimeError('Updating version failed with status code: {0}'
+                           .format(response.status_code))
+
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('subject', type = str,
-                        help = 'Bintray account.')
-    parser.add_argument('repo', type = str,
-                        help = 'Bintray repository.')
-    parser.add_argument('package', type = str,
-                        help = 'Bintray package.')
-    parser.add_argument('version', type = str,
-                        help = 'Bintray version.')
-    parser.add_argument('file_input', type = str,
-                        help = 'File to upload.')
-    parser.add_argument('username', type = str,
-                        help = 'Bintray username.')
-    parser.add_argument('api_key', type = str,
-                        help = 'Bintray API key.')
-    parser.add_argument('--filepath', type = str,
-                        help = 'Bintray file path (default: file input '
-                               'basename).')
-    parser.add_argument('--publish', action='store_true',
-                        help = 'Publish the uploaded artifact as part of '
-                               'uploading.')
-    parser.add_argument('--override', action='store_true',
-                        help = 'Overwrite already published artifact.')
-    parser.add_argument('--explode', action='store_true',
-                        help = 'Unknown behavior.')
+    parser.add_argument('--username', type = str,
+                        help = 'bintray username (default: value from '
+                               '"bintray_username" environment variable.')
+    parser.add_argument('--api_key', type = str,
+                        help = 'bintray API key (default: value from '
+                               '"bintray_api_key" environment variable.')
+
+    subparsers = parser.add_subparsers(
+        title='subcommands',
+        help='list of subcommands')
+
+    # Bintray upload
+    parser_upload = subparsers.add_parser(
+        'upload',
+        help='upload to bintray'
+    )
+    parser_upload.add_argument('repo', type = str,
+                               help = 'bintray repository.')
+    parser_upload.add_argument('package', type = str,
+                               help = 'bintray package.')
+    parser_upload.add_argument('version', type = str,
+                               help = 'bintray version.')
+    parser_upload.add_argument('file_input', type = str,
+                               help = 'file to upload.')
+    parser_upload.add_argument('--subject', type = str,
+                               help = 'bintray account (default: username).')
+    parser_upload.add_argument('--filepath', type = str,
+                               help = 'bintray file path (default: file input '
+                                      'basename).')
+    parser_upload.add_argument('--publish', action='store_true',
+                               help = 'publish the uploaded artifact as part '
+                                      'of uploading.')
+    parser_upload.add_argument('--override', action='store_true',
+                               help = 'overwrite already published artifact.')
+    parser_upload.add_argument('--explode', action='store_true',
+                               help = 'unknown behavior.')
+    parser_upload.set_defaults(func=bintray_upload)
+
+    # Publish content
+    parser_publish = subparsers.add_parser(
+        'publish',
+        help='publish uploaded content'
+    )
+    parser_publish.add_argument('repo', type = str,
+                                help = 'bintray repository.')
+    parser_publish.add_argument('package', type = str,
+                                help = 'bintray package.')
+    parser_publish.add_argument('version', type = str,
+                                help = 'bintray version.')
+    parser_publish.add_argument('--subject', type = str,
+                                help = 'bintray account (default: username).')
+    parser_publish.add_argument('--discard', action = 'store_true',
+                                help = 'discard uploaded content instead of '
+                                       'publishing it.')
+    parser_publish.set_defaults(func=bintray_publish)
+
+    # Add or remove a file in download list
+    parser_download_list = subparsers.add_parser(
+        'download-list',
+        help='add or remove a file in download list'
+    )
+    parser_download_list.add_argument('repo', type = str,
+                                      help = 'bintray repository.')
+    parser_download_list.add_argument('filepath', type = str,
+                                      help = 'bintray file path.')
+    parser_download_list.add_argument('action', type = str,
+                                      choices = ['add', 'remove'],
+                                      default = 'add',
+                                      help = 'add or remove file '
+                                             '(default: add).')
+    parser_download_list.add_argument('--subject', type = str,
+                                      help = 'bintray account '
+                                             '(default: username).')
+    parser_download_list.set_defaults(func=bintray_file_in_download_list)
+
+    # Delete version
+    parser_delete_version = subparsers.add_parser(
+        'delete-version',
+        help='delete the specified version.'
+    )
+    parser_delete_version.add_argument('repo', type = str,
+                                       help = 'bintray repository.')
+    parser_delete_version.add_argument('package', type = str,
+                                       help = 'bintray package.')
+    parser_delete_version.add_argument('version', type = str,
+                                       help = 'bintray version.')
+    parser_delete_version.add_argument('--subject', type = str,
+                                       help = 'bintray account '
+                                              '(default: username).')
+    parser_delete_version.set_defaults(func=delete_version)
+
+    # Update version
+    parser_update_version = subparsers.add_parser(
+        'update-version',
+        help='update the information of the specified version.'
+    )
+    parser_update_version.add_argument('repo', type = str,
+                                       help = 'bintray repository.')
+    parser_update_version.add_argument('package', type = str,
+                                       help = 'bintray package.')
+    parser_update_version.add_argument('version', type = str,
+                                       help = 'bintray version.')
+    parser_update_version.add_argument('desc', type = str,
+                                       help = 'version description.')
+    parser_update_version.add_argument('--subject', type = str,
+                                       help = 'bintray account '
+                                              '(default: username).')
+    parser_update_version.add_argument('--github-release-notes-file',
+                                       type = str,
+                                       help = 'GitHub release notes file.')
+    parser_update_version.add_argument('--github-use-tag-release-notes',
+                                       type = str,
+                                       help = 'GitHub use tag release notes.')
+    parser_update_version.add_argument('--vcs-tag', type = str,
+                                       help = 'VCS tag.')
+    parser_update_version.add_argument('--released', type = str,
+                                       help = 'release date.')
+    parser_update_version.set_defaults(func=update_version)
 
     args = parser.parse_args()
 
-    args.filepath = ( args.filepath if args.filepath else
-                      os.path.basename(args.file_input) )
+    if not args.username:
+        if 'bintray_username' not in os.environ:
+            raise RuntimeError('"bintray_username" environment '
+                               'variable not set')
+        args.username = os.environ['bintray_username']
+    if not args.api_key:
+        if 'bintray_api_key' not in os.environ:
+            raise RuntimeError('"bintray_api_key" environment '
+                               'variable not set')
+        args.api_key = os.environ['bintray_api_key']
 
-    return args
+    if not args.subject:
+        args.subject = args.username
+
+    args.func(args)
 
 
 def main():
-    args = parse_arguments()
-    upload_to_bintray(args)
+    parse_arguments()
 
 
 if __name__ == '__main__':
